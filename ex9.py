5e4846412e26c60d33e380e159a7397c1c57aee2
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-09-20 14:55:26 -0300

    ex 9 completo
    maior confusão, mas deu certo xD

--- ex9.py
+++ ex9.py
@@ -4,6 +4,10 @@
 # 2. uma lista com os 5 últimos números;
 # 3. uma lista contendo apenas os elementos das posições pares;
 # 4. uma lista contendo apenas os elementos das posições ímpares;
+# 5. a lista inversa da lista sorteada (isto é, uma lista que começa com o último elemento da
+# lista sorteada e termina com o primeiro);
+# 6. uma lista inversa dos 5 primeiros números;
+# 7. uma lista inversa dos 5 últimos números.
 
 lista = [2, 4, 5, 7, 2, 4, 1, 3, 4, 8]
 i = 0
@@ -31,3 +35,29 @@ print('Cinco últimos elementos: ', lista[-5:])
 print('Elementos nas posições pares: ', lista[::2])
 print('Elementos nas posições ímpares: ', lista[1::2])
 # muito mais fácil
+from random import randint
+
+def gerar_lista_aleatoria():
+  lista_aleatoria = []
+  i=0
+  for i in range(10):
+    lista_aleatoria.append(randint(1, 100))
+  
+  return lista_aleatoria
+
+randomList = gerar_lista_aleatoria()
+
+print(lista)
+lista.reverse()
+print(lista)
+lista.reverse()
+print(lista)
+fiveFirst = lista[:5]
+lastFive = lista[-5:]
+fiveFirst.reverse()
+fiveFirst.extend(lastFive)
+print('5 primeiros emabralhados', fiveFirst)
+lastFive.reverse()
+fiveFirst = lista[:5]
+fiveFirst.extend(lastFive)
+print('5 primeiros emabralhados', fiveFirst)
